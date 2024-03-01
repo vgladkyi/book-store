3c09ed38beac0d959f134d08cce4c1cc9d448796
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2024-03-01 11:12:42 +0200

    initial commit

---
+++ src/main/java/bookstore/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,52 @@
+package bookstore.repository.impl;
+
+import bookstore.model.Book;
+import bookstore.repository.BookRepository;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+
+    private final SessionFactory sessionFactory;
+
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save book to DB" + e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery(
+                    "SELECT Book FROM Book ", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get books from DB", e);
+        }
+    }
+}
---
+++ src/main/java/bookstore/service/impl/BookServiceImpl.java
@@ -0,0 +1,27 @@
+package bookstore.service.impl;
+
+import bookstore.model.Book;
+import bookstore.repository.BookRepository;
+import bookstore.service.BookService;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
