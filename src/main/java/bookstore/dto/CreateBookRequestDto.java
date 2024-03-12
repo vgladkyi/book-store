893d3ca4326c00711cff4c98761c497d107566e6
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2024-03-12 12:36:12 +0200

    snapshots update

---
+++ src/main/java/bookstore/dto/CreateBookRequestDto.java
@@ -0,0 +1,20 @@
+package bookstore.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Data
+public class CreateBookRequestDto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
