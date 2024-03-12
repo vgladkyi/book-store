8e41a8d10a69a6e05eacbc0fa52c1b3524b42b2a
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2024-03-12 09:34:00 +0200

    dtos, controller were added. New methods in service and repository were implemented

--- src/main/java/bookstore/BookStoreApplication.java
+++ src/main/java/bookstore/BookStoreApplication.java
@@ -1,38 +1,12 @@
 package bookstore;
 
-import bookstore.model.Book;
-import bookstore.service.BookService;
-import java.math.BigDecimal;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
-    private final BookService bookService;
-
-    @Autowired
-    public BookStoreApplication(BookService bookService) {
-        this.bookService = bookService;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book book = new Book();
-            book.setAuthor("Osama Dazai");
-            book.setTitle("Title");
-            book.setIsbn("isbn");
-            book.setPrice(BigDecimal.TEN);
-            book.setCoverImage("image");
-            book.setDescription("description");
-            bookService.save(book);
-        };
-    }
 }
