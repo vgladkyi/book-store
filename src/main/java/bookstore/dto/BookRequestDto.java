a7717ff48886ad0abd59ae47a7190d8211c50c58
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2024-05-16 22:03:39 +0300

    annotation @NotBlank were added

--- src/main/java/bookstore/dto/BookRequestDto.java
+++ src/main/java/bookstore/dto/BookRequestDto.java
@@ -1,17 +1,17 @@
 package bookstore.dto;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class BookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
+    @NotBlank
     private String isbn;
     @Min(0)
     private BigDecimal price;
