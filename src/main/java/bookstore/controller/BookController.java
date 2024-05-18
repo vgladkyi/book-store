bbec1ea937128c290b10767edf08e1803bf5f501
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2024-05-18 11:33:30 +0300

    @ParameterObject @PageableDefault were added. File application.properties were modified.

--- src/main/java/bookstore/controller/BookController.java
+++ src/main/java/bookstore/controller/BookController.java
@@ -11,7 +11,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Book management", description = "Endpoints for managing books")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/api/books")
+@RequestMapping(value = "/books")
 public class BookController {
     private final BookService bookService;
     private final BookMapper bookMapper;
@@ -34,7 +36,7 @@ public class BookController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Display all available books", description = "Display all available books")
-    public List<BookDto> findAll(Pageable pageable) {
+    public List<BookDto> findAll(@ParameterObject @PageableDefault Pageable pageable) {
         return bookService.findAll(pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
@@ -74,7 +76,9 @@ public class BookController {
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Display books by filter and with sorting",
             description = "Display books by filter and with sorting")
-    public List<BookDto> searchBooks(BookSearchParametersDto searchParameters, Pageable pageable) {
+    public List<BookDto> searchBooks(
+            @ParameterObject @PageableDefault BookSearchParametersDto searchParameters,
+            Pageable pageable) {
         return bookService.search(searchParameters, pageable)
                 .stream()
                 .map(bookMapper::toDto)
